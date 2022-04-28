8b5645eadcef919b408c4906db341d7ad0d687da
Ismael Collado <ismacollado1@gmail.com>
2022-04-28 10:21:41 -0700

    add operators and make it ready to use it

--- Scripts/CalculatorJS.js
+++ Scripts/CalculatorJS.js
@@ -1,10 +1,16 @@
 let operatorSelected = ''
 let lastOperation = 0
 let currentOperation = ''
+let resultSolution = 0
+let haveDot = false
 const addButton = document.getElementById('operatorAdd')
 const numberButton = document.querySelectorAll('.numberButton')
 const operation = document.getElementsByClassName('operation')
 const operatorButton = document.querySelectorAll('.operatorButton')
+const resultButton = document.getElementById('resultButton')
+const resultText = document.getElementsByClassName('result')
+const clearButton = document.getElementsByClassName('clear')
+const delButton =   document.getElementsByClassName('delete')
 
 const addFunction = (num1, num2 ) => {
     return num1 + num2
@@ -24,9 +30,10 @@ const divideFunction = (num1, num2) => {
 
 numberButton.forEach((button) => {
     button.addEventListener('click', () => {
-        if ((operation[0].innerHTML != '0') || (operation[0].innerHTML == '0' &&  button.innerHTML == '.') ){
+        if ((operation[0].innerHTML != '0') || (operation[0].innerHTML == '0' &&  button.innerHTML == '.' && haveDot == false) ){
             currentOperation += button.innerHTML
             operation[0].innerHTML += button.innerHTML
+            if (button.innerHTML == '.') {haveDot = true}
         }
         else{
             currentOperation = button.innerHTML
@@ -35,11 +42,60 @@ numberButton.forEach((button) => {
     })
 })
 
+
+
+const operate = (num1, num2, operator) => {
+    operationResult = 0
+    switch (operator) {
+        case '+':
+            operationResult = addFunction(num1, num2)
+            break;
+        case '-':
+            operationResult = subtractFunction(num1, num2)
+            break;
+        case '*':
+            operationResult = multiplyFunction(num1, num2)
+            break;
+        case '/':
+            operationResult = divideFunction(num1,num2)
+    }
+
+    return operationResult
+} 
+
 operatorButton.forEach((button) =>{
     button.addEventListener('click', () => {
         if (operatorSelected === ''){
             operatorSelected = button.innerHTML
             operation[0].innerHTML += operatorSelected
         }
+        if (resultSolution !== 0){
+            operatorSelected = button.innerHTML
+            resultText[0].innerHTML = operate(parseInt(resultSolution), parseInt(currentOperation), operatorSelected)
+            resultSolution = resultText[0].innerHTML
+            operation[0].innerHTML = 0
+        }
     })
+})
+
+function ShowResult(){
+    let nums = operation[0].innerHTML.split(operatorSelected)
+    resultText[0].innerHTML = operate(parseInt(nums[0]),parseInt(nums[1]), operatorSelected)
+    resultSolution = resultText[0].innerHTML
+    operation[0].innerHTML = 0
+}
+
+resultButton.addEventListener('click', ShowResult)
+
+clearButton[0].addEventListener('click', () => {
+    operatorSelected = ''
+    lastOperation = 0
+    currentOperation = ''
+    resultSolution = 0
+    operation[0].innerHTML = 0
+    resultText[0].innerHTML = ''
+})
+
+delButton[0].addEventListener('click', () => {
+    operation[0].innerHTML = operation[0].innerHTML.slice(0, -1)
 })
